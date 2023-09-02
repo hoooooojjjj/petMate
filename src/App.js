ce2e1cf59ab86bed91d6dcd5cd848227f7388ccd
hoooooojjjj <rhj080471@gmail.com>
2023-09-03 01:13:12 +0900

    회원가입 틀 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SignUp from "./page/SignUp";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
