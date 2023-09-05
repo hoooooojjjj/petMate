9b4ad20e0e17db03ecb7dc19ddaa440c1fdc7b15
hoooooojjjj <rhj080471@gmail.com>
2023-09-05 22:12:54 +0900

    회원가입 / 로그인 구현

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import Signup from "./components/Signup.js";
 import MainPage from "./components/MainPage.js";
 import NotFound from "./components/NotFound.js";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Myfirebase.js";
 function App() {
+  // 사용자 정보(=> props로 전달하면 user.displayName(유저 닉네임), user.uid(유저 토큰) 등을 가져갈 수 있음)
+  const [user, setuser] = useState(null);
+  // 사용자가 로그인 되어있는지 아닌지(=>props으로 전달하면 로그인 됐을때 안됐을 때 구분가능)
+  const [isLogin, setisLogin] = useState(false);
+
+  // 컴포넌트가 마운트(첫 렌더링)됐을 때 사용자가 로그인되어 있는지 확인
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setuser((args) => user);
+        console.log(user); // -> 로그인 되어 있는 유저에 대한 정보가 들어있음(user.displayName => 유저 닉네임)
+        setisLogin(true); // 로그인됨.
+      } else {
+        setisLogin(false); // 로그인 안됨.
+      }
+    });
+  }, []);
   return (
     <Router>
       <Routes>
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,27 +1,67 @@
-import React from "react";
-import { TextField, Checkbox, Button, FormControlLabel, Grid,
-  Typography, Avatar, Box, Container } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { useNavigate,Link  } from "react-router-dom";
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Avatar,
+  Box,
+  Container,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../Myfirebase";
 
 export default function Signin() {
   const navigate = useNavigate();
-  const login = () => {
-    //로그인 로직이 필요합니다.
+  const [signUp, setsignUp] = useState({
+    email: "",
+    password: "",
+  }); // 이메일, 비밀번호 입력값
 
-    navigate("/MainPage");
-  }
+  // 이메일 및 비밀번호 input change 됐을 때
+  const handleChange = (e) => {
+    setsignUp({
+      ...signUp,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // 로그인
+  const onSignIn = () => {
+    signInWithEmailAndPassword(auth, signUp.email, signUp.password)
+      .then((userCredential) => {
+        // Signed in
+        navigate("/MainPage");
+      })
+      .catch((error) => {});
+  };
+
+  // 구글 로그인
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        navigate("/MainPage");
+      })
+      .catch((error) => {});
+  };
   return (
     <Container component="main" maxWidth="xs">
       <Box
         sx={{
           marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }} >
-        <Avatar
-          sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
@@ -32,8 +72,11 @@ export default function Signin() {
           label="아이디"
           required
           fullWidth
-          name="id"
-          autoFocus />
+          name="email"
+          autoFocus
+          value={signUp.email}
+          onChange={handleChange}
+        />
         <TextField
           margin="normal"
           label="비밀번호"
@@ -41,25 +84,33 @@ export default function Signin() {
           required
           fullWidth
           name="password"
-          autoComplete="current-password" />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="아이디 저장" />
-
+          autoComplete="current-password"
+          value={signUp.password}
+          onChange={handleChange}
+        />
         <Button
           type="submit"
           fullWidth
           variant="contained"
-          onClick={login}
-          sx={{ mt: 3, mb: 2 }} >
+          onClick={onSignIn}
+          sx={{ mt: 3, mb: 2 }}
+        >
           로그인
         </Button>
+        <Button
+          style={{ marginBottom: 20 }}
+          type="submit"
+          fullWidth
+          variant="contained"
+          onClick={googleSignIn}
+        >
+          구글로 로그인
+        </Button>
         <Grid container>
-          <Grid item xs>
-            <Link>비밀번호 찾기</Link>
-          </Grid>
           <Grid item>
-            <Link to="/signup">회원가입</Link>
+            <Link style={{ marginLeft: 170 }} to="/signup">
+              회원가입
+            </Link>
           </Grid>
         </Grid>
       </Box>
